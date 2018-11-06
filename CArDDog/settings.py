f73e6fd26d061d1627def57dcaddb245721434e7
YutoTamura <yuto.tamur@gmail.com>
2018-11-06 17:48:05 +0900

    [add] docker用の色々
    * dbをpostgresqlに
    * Dockerfileを作成

--- CArDDog/settings.py
+++ CArDDog/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'CArDDog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
