438dc1182e99bb4733c66e0d32fea5f25cb8d0d3
YutoTamura <yuto.tamur@gmail.com>
2019-02-21 19:14:13 +0900

    [add] profile編集用APIを追加

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login', views.loginview.as_view(), name="login"),
     path('signup', views.signupview.as_view(), name="singup"),
     path('user/', include('social_django.urls', namespace='social')),
-    path('face/', views.findface.as_view(), name='hoge')
+    path('face/', views.findface.as_view(), name='hoge'),
+    path('profile/', views.profile.as_view(), name='profile')
 ]
--- api/views.py
+++ api/views.py
@@ -4,11 +4,13 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from .models import Profile
+from social_django.models import UserSocialAuth
+
 import os
 import uuid
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../CArDCat'))
-import imagepredict
 from keras import backend as K
 # Create your views here
 
@@ -51,8 +53,49 @@ class findface(APIView):
         p = imagepredict.ImagePredictor()
         # print(p.predict(path))
         return Response(p.predict(path))
-        """
         a, base64encdjpg = requeset.data['file'].split(';base64,') 
         K.clear_session()
         p = imagepredict.ImagePredictor()
         return Response(p.predict_from_base64img(base64encdjpg))
+        """
+        return Response('工事中')
+class profile(APIView):
+    def post(self, request):
+        req_type = request.query_params["req_type"]
+        if req_type == "icon":
+            user = request.user
+            profile, _ = Profile.objects.get_or_create(user=user)
+            profile.user_icon_url = _get_icon_from_user(user)
+            profile.save()
+            return Response('success')
+        elif req_type == "name":
+            user = request.user
+            profile, _ = Profile.objects.get_or_create(user=user)
+            profile.display_name = request.data["name"]
+            profile.save()
+            return Response('success')
+        elif req_type == "dict":
+            user = request.user
+            profile, _ = Profile.objects.get_or_create(user=user)
+            profile.user_dictionaly = request.data["dict"]
+            profile.save()
+            return Response('success')
+        else:
+            return Response('failed')
+    def get(self, requeset):
+        user = requeset.user
+        profile, _ = Profile.objects.get_or_create(user=user)
+        icon = profile.user_icon_url
+        name = profile.display_name
+        dict_d = profile.user_dictionaly
+        return Response({"icon": icon, "name": name, "dict": dict_d})
+
+def _get_icon_from_user(user):
+    social_user = UserSocialAuth.objects.get(user=user)
+    provider = social_user.provider
+    if provider == "github":
+        print(social_user.extra_data)
+        return "https://github.com/{}.png".format(social_user.extra_data["login"])
+    elif provider == "twitter":
+        pass
+    return ''
